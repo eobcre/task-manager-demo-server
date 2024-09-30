7ce12be1154120cc1100db20f99d00a5e0f97532
Emmy Manning <emswoo@EM.local>
2024-09-30 16:46:31 -0400

    custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: '#4f55c4',
+      },
+    },
   },
   plugins: [],
 };
