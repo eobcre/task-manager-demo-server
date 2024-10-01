970a84bbab330e89f66e518d0b3ccdde7d7c5e82
Emmy Manning <emswoo@EM.local>
2024-10-01 19:06:09 -0400

    fixed nav icons position

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -14,8 +14,8 @@ const Nav = () => {
     <div className={`bg-purple duration-500 h-screen ${isNavOpen ? 'w-80' : 'w-20'}`} onClick={handleClickNavOpen}>
       <div className='flex flex-col justify-center gap-10 py-5'>
         <nav className='relative flex flex-col gap-10'>
-          <Icon icon='fluent-emoji-high-contrast:raccoon' className='absolute left-[1.2rem] text-white w-[40px] h-[40px]' />
-          <div className='absolute top-[6rem] left-[1.5rem] flex flex-col justify-center gap-10 cursor-pointer'>
+          <Icon icon='fluent-emoji-high-contrast:raccoon' className='absolute left-[1rem] text-white w-[40px] h-[40px]' />
+          <div className='absolute top-[6rem] left-[1.2rem] flex flex-col justify-center gap-10 cursor-pointer'>
             {navData.map((item: NavItemProps) => (
               <Link key={item.id} to={item.link}>
                 <div className='flex items-center gap-6'>
