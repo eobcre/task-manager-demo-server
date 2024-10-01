bc0bede680807dc1b682490f3169c03143f338e8
Emmy Manning <emswoo@EM.local>
2024-10-01 19:50:32 -0400

    active nav link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,36 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 import { navData, NavItemProps } from '../data/navData';
 
 const Nav = () => {
   const [isNavOpen, setNavOpen] = useState(false);
 
-  const handleClickNavOpen = () => {
-    setNavOpen((prev) => !prev);
+  const handleClickNavOpen = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLElement;
+    if (target.closest('.nav-items') || target.closest('.home-icon')) {
+      return;
+    }
+    if (target.classList.contains('nav-container')) {
+      setNavOpen((prev) => !prev);
+      return;
+    }
+    setNavOpen(true);
   };
 
   return (
-    <div className={`bg-purple duration-500 h-screen ${isNavOpen ? 'w-80' : 'w-20'}`} onClick={handleClickNavOpen}>
+    <div className={`nav-container bg-purple duration-500 h-screen ${isNavOpen ? 'w-80' : 'w-20'}`} onClick={handleClickNavOpen}>
       <div className='flex flex-col justify-center gap-10 py-5'>
         <nav className='relative flex flex-col gap-10'>
-          <Icon icon='fluent-emoji-high-contrast:raccoon' className='absolute left-[1rem] text-white w-[40px] h-[40px]' />
-          <div className='absolute top-[6rem] left-[1.2rem] flex flex-col justify-center gap-10 cursor-pointer'>
+          <Icon icon='fluent-emoji-high-contrast:raccoon' className='home-icon absolute left-[1rem] text-white w-[40px] h-[40px]' />
+          <div className='absolute top-[6rem] flex flex-col justify-center cursor-pointer w-full'>
             {navData.map((item: NavItemProps) => (
-              <Link key={item.id} to={item.link}>
+              <NavLink key={item.id} to={item.link} className={({ isActive }) => `nav-items px-5 py-5 w-full ${isActive ? 'bg-darkPurple' : ''}`}>
                 <div className='flex items-center gap-6'>
                   <Icon icon={item.iconName} className={item.style} />
                   {isNavOpen && <span className='text-white'>{item.nav}</span>}
                 </div>
-              </Link>
+              </NavLink>
             ))}
           </div>
         </nav>
