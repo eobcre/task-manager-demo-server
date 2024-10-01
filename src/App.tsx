efe3121da71b4e52bf306b497717f326f4d299e4
Emmy Manning <emswoo@EM.local>
2024-10-01 16:17:48 -0400

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import useStore from './store/useStore';
 import Nav from './components/Nav';
+import Tasks from './pages/Tasks';
 
 const App = () => {
+  const { isNavOpen } = useStore();
+
   return (
-    <div>
-      <div>
-        <Nav />
-      </div>
+    <div className='flex'>
+      <Nav />
+      <main className={`w-full ${isNavOpen ? '330px' : '65px'}`}>
+        <Routes>
+          <Route path='/Tasks' element={<Tasks />} />
+        </Routes>
+      </main>
     </div>
   );
 };
