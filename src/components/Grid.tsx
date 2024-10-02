1b20e699c4d88e3483478acd3b8e6bd56b83a81f
Emmy Manning <emswoo@EM.local>
2024-10-02 12:45:12 -0400

    updated grid row

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,7 +4,7 @@ const Grid = () => {
   return (
     <div className='border border-slate-500 rounded-md w-full h-auto'>
       {/* header row */}
-      <div className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr_0.6fr] text-white font-bold bg-purple opacity-90 border-b border-slate-400 p-4'>
+      <div className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr_0.4fr] text-white font-bold bg-purple opacity-90 border-b border-slate-400 p-4'>
         <div>Task</div>
         <div>Assigned By</div>
         <div>Document Type</div>
@@ -12,7 +12,7 @@ const Grid = () => {
         <div></div>
       </div>
       {/* data row */}
-      <div className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr_0.6fr] text-dark border-b border-slate-300 p-4'>
+      <div className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr_0.4fr] text-dark border-b border-slate-300 p-4'>
         <div>Review</div>
         <div>John Doe</div>
         <div>Application</div>
@@ -22,7 +22,7 @@ const Grid = () => {
         </div>
       </div>
       {/* data row*/}
-      <div className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr_0.6fr] text-dark border-b border-slate-300 p-4'>
+      <div className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr_0.4fr] text-dark border-b border-slate-300 p-4'>
         <div>Approval</div>
         <div>Jane Smith</div>
         <div>Contract</div>
